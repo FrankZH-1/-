bb3ec6856b473c8d5b629b352abf8034a8358c58
Zhao_Yifei <chinacpld@126.com>
2021-03-21 23:42:31 +0800

    added animation.bug not fixed

--- frontPageScript.js
+++ frontPageScript.js
@@ -12,6 +12,16 @@
   doc.addEventListener('DOMContentLoaded',recalc,false);
 })(document,window);
 
-// (function(doc){
-//   var
-// })
+(function(doc){
+  var enterGame = doc.getElementsByClassName(".startButton .optionButton .buttonText"),
+  actionButtons = doc.getElementsByClassName('.functionButtons'),
+  enter = function(){
+    enterGame.style.animation = "runOut 0.5s";
+    actionButtons.style.animation = 'runIn 0.5s';
+  },
+  leave = function(){
+    element.style.animation = "runIn 0.5s";
+  };
+  if(!enterGame.addEventListener||!actionButtons.addEventListener) return;
+  enterGame.addEventListener('touchend',enter);  
+})(document)
