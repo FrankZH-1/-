39018a02d6da31af4b3ba8f7182fd1aa0c1ffde7
Zhao_Yifei <chinacpld@126.com>
2021-03-22 22:45:18 +0800

    ver 0.2, frontpage designed.voting system is designing.

--- frontPageScript.js
+++ frontPageScript.js
@@ -1,27 +1,46 @@
-(function(doc,win){
+(function (doc, win) {
   var doc1 = doc.documentElement,
-  spinEvt = 'orientationchange' in window ? 'orientationchange':'resize',
-  recalc = function(){
-    var clientWidth = doc1.clientWidth;
-    if (!clientWidth) return;
-    doc1.style.fontSize = 20*(clientWidth/320)+'px';
-  };
+    spinEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    recalc = function () {
+      var clientWidth = doc1.clientWidth;
+      if (!clientWidth) return;
+      doc1.style.fontSize = 20 * (clientWidth / 320) + 'px';
+    };
 
   if (!doc.addEventListener) return;
-  win.addEventListener(spinEvt,recalc,false);
-  doc.addEventListener('DOMContentLoaded',recalc,false);
-})(document,window);
+  win.addEventListener(spinEvt, recalc, false);
+  doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
 
-(function(doc){
-  var enterGame = doc.getElementsByClassName(".startButton .optionButton .buttonText"),
-  actionButtons = doc.getElementsByClassName('.functionButtons'),
-  enter = function(){
-    enterGame.style.animation = "runOut 0.5s";
-    actionButtons.style.animation = 'runIn 0.5s';
+window.onload = function () {
+  var startGame = document.querySelector(".startButton"),
+    functionButtons = document.querySelectorAll(".functionButtons a .optionButton"),
+    backButton = document.querySelector(".functionButtons a:last-child"),
+  clickStart = function () {
+    startGame.style.animation = "moveOut 0.4s forwards";
+    (function () {
+      for (let i = 0; i < functionButtons.length; i++) {
+        setTimeout(() => {
+          functionButtons[i].style.animation = "moveIn 0.4s forwards";
+        }, i * 50);
+      }
+    }
+    )();
   },
-  leave = function(){
-    element.style.animation = "runIn 0.5s";
+  clickBack = function () {
+    startGame.style.animation = "moveIn 0.4s forwards";
+    (function () {
+      for (let i = 0; i < functionButtons.length; i++) {
+        setTimeout(() => {
+          functionButtons[i].style.animation = "moveOut 0.4s forwards";
+        }, i * 50);
+      }
+    }
+    )();
   };
-  if(!enterGame.addEventListener||!actionButtons.addEventListener) return;
-  enterGame.addEventListener('touchend',enter);  
-})(document)
+  // console.log(functionButtons);
+  // console.log(backButton);
+  startGame.addEventListener("touchend", clickStart);
+  backButton.addEventListener("touchend", clickBack);
+
+}
